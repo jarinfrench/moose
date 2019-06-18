67847442ac8240f2c170c4aa9ff10ed5ea323d0a
Jarin French <jarin.french@inl.gov>
2019-06-18 16:27:48 -0600

    Continuing to work on the anisotropy provider.

--- modules/phase_field/include/userobjects/RandomBoundaryAnisotropyProvider.h
+++ modules/phase_field/include/userobjects/RandomBoundaryAnisotropyProvider.h
@@ -33,8 +33,22 @@ public:
   virtual unsigned int getGrainNum() const override;
 
 protected:
+  std::vector<std::vector<Real> > generatePropertyMatrix(unsigned int size, unsigned int num_to_change, Real iso_value, Real aniso_value);
   const GrainTrackerInterface & _grain_tracker;
   std::vector<std::vector<Real> > _energies, _mobilities, _qs;
+  
+  /// Isotropic values
+  Real _iso_energy, _iso_mobility, _iso_q;
+  
+  /// Anisotropic values
+  Real _aniso_energy, _aniso_mobility, _aniso_q;
+  
+  unsigned int _seed; // random number generator seed
+  unsigned int _n; // number of grain boundaries to change properties of
+  
+  Real _fraction; // fraction of grain boundaries to change properties of
+  
+  MultiMooseEnum _anisotropies; // The properties to assign anisotropic values to
 
   MooseRandom _random;
 }
--- modules/phase_field/src/userobjects/RandomBoundaryAnisotropyProvider.C
+++ modules/phase_field/src/userobjects/RandomBoundaryAnisotropyProvider.C
@@ -9,7 +9,8 @@
 
 #include "RandomBoundaryAnisotropyProvider.h"
 #include "GrainTrackerInterface"
-#include "MooseEnum.h"
+#include "MultiMooseEnum.h"
+#include "MathUtils.h"
 
 registerMooseObject("PhaseFieldApp", RandomBoundaryAnisotropyProvider);
 
@@ -18,11 +19,11 @@ InputParameters
 validParams<RandomBoundaryAnisotropyProvider>()
 {
   InputParameters params = validParams<RandomBoundaryAnisotropyProvider>();
-  MooseEnum anisotropies("e m q", "e");
+  MultiMooseEnum anisotropies("e=0 m=1 q=2", "e");
   params.addClassDescription("Assign anisotropic boundary properties to a polycrystalline system");
   params.addRequiredParam<UserObjectName>("grain_tracker_object",
                                           "The FeatureFloodCount UserObject to get values from");
-  params.addParam<Real>("iso_energy", <value for copper>, 
+  params.addParam<Real>("iso_energy", <value for copper>,
                         "Isotropic grain boundary energy that will be assigned to all boundaries");
   params.addParam<Real>("iso_mobility", <value for copper>,
                         "Isotropic grain boundary mobility that will be assigned to all boundaries");
@@ -31,17 +32,26 @@ validParams<RandomBoundaryAnisotropyProvider>()
   params.addParam<unsigned int>("seed", 0, "Seed value for the random number generator");
   params.addRangeCheckedParam<Real>("fraction", 0.0, "fraction>=0 & fraction<=1",
                                     "The fraction of boundaries that will be assigned anisotropic values");
-  params.addParam<unsigned int>("n", 0, "The number of boundaries to assign anisotropic properties to");
-  params.addParam<MooseEnum>("anisotropies", anisotropies, "The boundary properties that will be made anisotropic");
+  params.addParam<unsigned int>("n", "The number of boundaries to assign anisotropic properties to");
+  params.addParam<MultiMooseEnum>("anisotropies", anisotropies, "The boundary properties that will be made anisotropic");
   params.addParam<Real>("aniso_energy", "The anisotropic grain boundary energy that will be assigned to the anisotropic boundaries");
   params.addParam<Real>("aniso_mobility", "The anisotropic grain boundary mobility that will be assigned to the anisotropic boundaries");
   params.addParam<Real>("aniso_q", "The anisotropic activation energy that will be assigned to the anisotropic boundaries");
-  
+
   return params;
 }
 
 RandomBoundaryAnisotropyProvider::RandomBoundaryAnisotropyProvider(const InputParameters & params)
-  : _grain_tracker(getUserObject<GrainTrackerInterface>("grain_tracker_object"))
+  : _grain_tracker(getUserObject<GrainTrackerInterface>("grain_tracker_object")),
+    _iso_energy(getParam<Real>("iso_energy")),
+    _iso_mobility(getParam<Real>("iso_mobility")),
+    _iso_q(getParam<Real>("iso_q")),
+    _seed(getParam<unsigned int>("seed")),
+    _fraction(getParam<Real>("fraction")),
+    _anisotropies(getParam<MultiMooseEnum>("anisotropies")),
+    _aniso_energy(getParam<Real>("aniso_energy")),
+    _aniso_mobility(getParam<Real>("aniso_mobility")),
+    _aniso_q(getParam<Real>("aniso_q"))
 {
   _random.seed(0, getParam<unsigned int>("seed"));
 }
@@ -58,13 +68,46 @@ void RandomBoundaryAnisotropyProvider::initialize()
     }
   }
 
-  if (isParamValid("fraction")) // something checking if fraction was specified vs the number
+  if (isParamSetByUser("n"))
   {
-    // determine the number of boundaries to change
+    _n = getParam<unsigned int>("n");
   }
-  
+  else
+  {
+    _n = MathUtils::round(_fraction * grain_num);
+  }
+
+  if (_n < 1)
+  {
+    mooseError("The number of grains to assign anisotropic values to must be >= 1");
+  }
+
+  if (_n == grain_num) // All values are changed from isotropic values
+  {
+    for (const auto & anisotropy : _anisotropies)
+    {
+      auto aniso_id = anisotropy.id();
+
+      switch (aniso_id)
+      {
+        case 0: // energy
+          //  function call with energy values
+          break;
+        case 1: // mobility
+          // function call with mobility values
+          break;
+        case 2: // activation energy
+          // function call with activation energy values
+          break;
+        default:
+          mooseError("Unknown anisotropy type ", aniso_id);
+      }
+      // do something here to specify the boundary properties - probably a function call
+    }
+  }
+
   // Will need something to actually give the values outside of the class... right?  see RandomEulerAngleProvider
-  
+
   // Is there a way the user can specify which boundaries they want to mark as anisotropic?  addParam<std::string>("vars", "The list of boundaries that are marked as anisotropic")?
-  
+
 }
